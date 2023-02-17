b0e395aaf964776eda0ab4f943f9acaa5b3c64ba
MuseOfSalzburg <durstonalistair@gmail.com>
2023-02-17 10:56:43 +0800

    Naming Fix

---
+++ app/player.py
@@ -0,0 +1,17 @@
+
+
+class Player:
+    def __init__(self, uid: str, player_name: str):
+        self._uid = uid
+        self._player_name = player_name
+
+    @property
+    def uid(self):
+        return self._uid
+
+    @property
+    def name(self):
+        return self._player_name
+
+    def __str__(self):
+        return f"Player name: {self._player_name} \n PLayer ID: {self._uid}"
---
+++ test/player_test.py
@@ -0,0 +1,17 @@
+import unittest
+from .app.player import Player
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_instance = Player("42", "C00L_G4M3R")
+
+    def test_uid_function(self):
+        self.assertEqual(self.test_instance.uid, "42")
+
+    def test_name_function(self):
+        self.assertEqual(self.test_instance.name, "C00L_G4M3R")
+
+
+if __name__ == '__main__':
+    unittest.main()
